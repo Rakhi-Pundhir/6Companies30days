ff994eafe7a6b66de54240cee2b10872fbbfc714
Rakhi Pundhir <rakhipundhir2711@gmail.com>
2022-01-02 13:04:41 +0530

    Company 1 quest2

---
+++ project/6Companies30days/C1q2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+    if((L1[0]==R1[0]) || (L1[1]==R1[1]) || (L2[0]==R2[0]) || (L2[1]==R2[1]))
+       return 0;
+       if((R1[0]<=L2[0]) || (R2[0]<=L1[0]))
+       return 0;
+       if((R2[1]>=L1[1]) || (R1[1]>=L2[1]))
+       return 0;
+       
+       return 1;
+    }
+};
